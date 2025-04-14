1f3650d337642ed9bbb86a32f9e4b2865f4cab0a
Jigarkumar Raval <jigarkumar1230@gmail.com>
2025-04-14 10:29:30 +0530

    update

--- backend/new1.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-function generateJWTSecret() {
-  const secret = crypto.randomBytes(64).toString("hex");
-  console.log("Generated JWT Secret:", secret);
-}
-
-generateJWTSecret();
